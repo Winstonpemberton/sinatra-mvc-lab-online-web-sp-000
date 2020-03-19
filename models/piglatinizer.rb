d95d61dea39d5630d59797ba5e7c5ccd532dc42f
Winston Pemberton <winstonpemberton@gmail.com>
2020-03-19 22:01:37 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,20 +1,25 @@
 class PigLatinizer
 
-  def is_vowel?(word)
-    vowels = 'aeiouAEIOU'
-    vowels.include?(word[0])
+  # def is_vowel?(word)
+  #   vowels = 'aeiouAEIOU'
+  #   vowels.include?(word[0])
+  # end
+  # 
+  #  def piglatinize(word)
+  #    if self.is_vowel?(word) == true
+  #      word.concat('way')
+  #    else
+  #      word.concat(word.slice!(/^[^aeiouAEIOU]*/i || "")) + 'ay'
+  #    end
+  #  end
+  # 
+  # 
+  #  def to_pig_latin(phrase)
+  #    phrase.split.collect {|word| piglatinize(word)}.join(' ')
+  #  end
+
+  def pig_latin(ay)
+    ay[0] =~ /[aeiou]/ ? ay.gsub(/([aeiou])(\w+)*/, '\1\2way') : ay.gsub(/([^aeiou])(\w+)*/, '\2\1ay')
   end
-
-   def piglatinize(word)
-     if self.is_vowel?(word) == true
-       word.concat('way')
-     else
-       word.concat(word.slice!(/^[^aeiouAEIOU]*/i || "")) + 'ay'
-     end
-   end
-
-
-   def to_pig_latin(phrase)
-     phrase.split.collect {|word| piglatinize(word)}.join(' ')
-   end
+  
 end
